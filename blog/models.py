1bb96599bb3a8818cbad924be4a89e9f3d0faead
Gourav <dkdgourav@gmail.com>
2017-10-24 22:24:38 +0530

    My Django app, first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+	"""docstring for Post"""
+	author=models.ForeignKey('auth.User')
+	title= models.CharField(max_length=40)
+	text=models.TextField()
+	created_date=models.DateTimeField(default=timezone.now)
+	published_date=models.DateTimeField(blank=True, null=True)
+	def publish(self):
+		self.publi
+	def __str__(self):
+		return self.title
+		
